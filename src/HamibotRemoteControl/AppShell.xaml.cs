78a467c3c48dc7da28bb3cda4a8a3d35f475886e
一刀 <yhonglai@163.com>
2023-07-31 00:15:03 +0800

    feat 添加icons

--- src/HamibotRemoteControl/AppShell.xaml.cs
+++ src/HamibotRemoteControl/AppShell.xaml.cs
@@ -4,9 +4,9 @@ namespace HamibotRemoteControl;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
+    public AppShell()
+    {
+        InitializeComponent();
         this.version.Text = $"版本号：{Software.Version}";
     }
 }
